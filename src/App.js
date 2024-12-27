d885c3af8c5d10e0cbe32ce080a62deb68a0b30b
vinyeee <yeevin@naver.com>
2024-12-28 00:44:35 +0900

    feat: navbar 추가

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { Navbar, Nav, Container } from 'react-bootstrap'
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
