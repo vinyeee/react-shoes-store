a660a9e1f689866d7cd50e545732206418d8c64e
vinyeee <yeevin@naver.com>
2024-12-29 03:03:08 +0900

    feat: detail  페이지 내용 추가

--- src/App.js
+++ src/App.js
@@ -41,7 +41,21 @@ function App() {
             </div> 
           </>
         } />
-        <Route path = "/detail"/>
+        <Route path = "/detail" element = {
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6">
+                <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+              </div>
+              <div className="col-md-6">
+                <h4 className="pt-5">상품명</h4>
+                <p>상품설명</p>
+                <p>120000원</p>
+                <button className="btn btn-danger">주문하기</button> 
+              </div>
+            </div>
+          </div>
+        }/>
       </Routes>
     </div>
   );
