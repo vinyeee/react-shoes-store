d06273ac3707dba87e7ca1296cba42767b3c54cb
vinyeee <yeevin@naver.com>
2024-12-29 02:59:23 +0900

    feat: 메인페이지, 상세페이지 라우팅 적용

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Navbar, Nav, Container } from 'react-bootstrap'
 import { useState } from 'react'
 import { shoes_data } from './data'
 import { ShoeItem } from './components'
+import { Routes, Route, Link } from 'react-router-dom'
 function App() {
 
   let [shoes] = useState(shoes_data);
   
   return (
     <div className="App">
+
       <Navbar bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
@@ -19,21 +21,28 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className='main-bg' style = {{backgroundImage : `url(${process.env.PUBLIC_URL + '/img/bg.png'})`}}></div>
 
-      <div className="container">
-        <div className="row">
-          {shoes.map((shoe) => {
-            return (
-              <ShoeItem 
-              title = {shoe.title}
-              price = {shoe.price}
-              imgUrl = {shoe.imgUrl}
-               />
-            );
-          })}
-        </div>
-      </div> 
+      <Routes>
+        <Route path = "/" element={
+          <>
+            <div className='main-bg' style = {{backgroundImage : `url(${process.env.PUBLIC_URL + '/img/bg.png'})`}}></div>
+            <div className="container">
+              <div className="row">
+                {shoes.map((shoe) => {
+                  return (
+                    <ShoeItem 
+                      title = {shoe.title}
+                      price = {shoe.price}
+                      imgUrl = {shoe.imgUrl}
+                    />
+                  );
+                })}
+              </div>
+            </div> 
+          </>
+        } />
+        <Route path = "/detail"/>
+      </Routes>
     </div>
   );
 }
