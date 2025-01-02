c4bd2754c42675a5ec91c3580f4f2423ff48df77
vinyeee <yeevin@naver.com>
2025-01-02 21:14:47 +0900

    feat: redux state가 object/array일 경우 수정 & payload

--- src/store.js
+++ src/store.js
@@ -9,8 +9,8 @@ let user = createSlice({
             // array/object의 경우 직접 수정해도 state 변경됨
           state.name = 'park'  
         },
-        changeAge(state){
-          state.age += 1
+        changeAge(state, action){
+          state.age += action.payload
         }
     }
 
